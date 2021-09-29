1a755aa9bb033473c001451b60b0d0456b168fae
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-09-29 16:57:45 +0900

    feat: 🎸 GitHub Actions を導入した

---
+++ lib/github_actions_scripts/install_firefox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+# - name: Firefox をインストールする
+#   run: |
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu focal main"
+sudo apt update
+sudo apt install -y firefox
---
+++ lib/github_actions_scripts/install_packages_for_cypress.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+
+# https://docs.cypress.io/guides/continuous-integration/introduction#Dependencies
+# name: Cypress の実行に必要なパッケージをインストールする
+# command: |
+sudo apt install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
