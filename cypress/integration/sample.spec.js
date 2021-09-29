fa93db12ca4db354081a6b2101f2985ba7972af0
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-09-29 19:17:53 +0900

    feat: 🎸 Cypress を導入した (#12)

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,10 @@
+describe('サンプルテスト', () => {
+  context('Yahoo!', () => {
+    it('トップページにアクセスできること', () => {
+      cy.visit('https://www.yahoo.co.jp/')
+
+      cy.location('origin').should('eq', 'https://www.yahoo.co.jp')
+      cy.location('protocol').should('eq', 'https:')
+    })
+  })
+})
