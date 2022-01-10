a3598216451d62725fe961ca13f1df5e38246a59
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-10 21:53:23 +0900

    feat: 🎸 ダウンロードスクリプトを本実装した

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,29 @@
 #!/usr/bin/env node
 
-// const pixHostDownloaderClient = require('')
-// const program = require('commander')
+const pixHostDownloaderClient = require('../src/app.js')
+const program = require('commander')
 
-// class PixHostDownloader {
-//   constructor() {
-//     program.option(
-//       '-u, --url <url>',
-//       'A image URL on PixHost',
-//     );
+class PixHostDownloader {
+  constructor() {
+    program
+      .option(
+        '-u, --url <url>',
+        'A image URL on PixHost',
+      )
+      .option(
+        '-d, --dir <directory>',
+        'A directory to save the image',
+        '/tmp',
+      )
 
-//     program.parse(process.argv)
-//     this.options = program.opts()
-//   }
+    program.parse(process.argv)
+    this.options = program.opts()
+  }
 
-//   execute() {
-//     pixHostDownloaderClient(this.options.url)
-//   }
-// }
+  execute() {
+    pixHostDownloaderClient(this.options.url, this.options.dir)
+  }
+}
 
-// const PixHostDownloader = new PixHostDownloader()
-// PixHostDownloader.execute()
-
-console.log('[DEBUG] PRINT DEBUG')
+const pixHostDownloader = new PixHostDownloader()
+pixHostDownloader.execute()
