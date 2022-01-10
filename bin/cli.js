52335d41f6d34443570c846094269751feea13de
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-01-10 21:31:48 +0900

    feat: 🎸 CLI 実行のための準備をした

---
+++ bin/cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// const pixHostDownloaderClient = require('')
+// const program = require('commander')
+
+// class PixHostDownloader {
+//   constructor() {
+//     program.option(
+//       '-u, --url <url>',
+//       'A image URL on PixHost',
+//     );
+
+//     program.parse(process.argv)
+//     this.options = program.opts()
+//   }
+
+//   execute() {
+//     pixHostDownloaderClient(this.options.url)
+//   }
+// }
+
+// const PixHostDownloader = new PixHostDownloader()
+// PixHostDownloader.execute()
+
+console.log('[DEBUG] PRINT DEBUG')
