9e1d2c519914f51ff6e9745dcf3dab9d459211d4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-12-05 02:40:56 +0900

    ci: 🎡 Firefox を snap版 から deb版 に戻す
    cf. https://yuzu441.hateblo.jp/entry/2022/05/05/180000

--- lib/github_actions_scripts/install_firefox.sh
+++ lib/github_actions_scripts/install_firefox.sh
@@ -2,7 +2,17 @@
 
 # - name: Firefox をインストールする
 #   run: |
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
-sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu jammy main"
+sudo snap remove --purge firefox
+sudo apt remove --autoremove firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+
+sudo echo "Package: firefox*" >> /etc/apt/preferences.d/99mozillateamppa
+sudo echo "Pin: release o=LP-PPA-mozillateam" >> /etc/apt/preferences.d/99mozillateamppa
+sudo echo "Pin-Priority: 501" >> /etc/apt/preferences.d/99mozillateamppa
+sudo echo "" >> /etc/apt/preferences.d/99mozillateamppa
+sudo echo "Package: firefox*" >> /etc/apt/preferences.d/99mozillateamppa
+sudo echo "Pin: release o=Ubuntu" >> /etc/apt/preferences.d/99mozillateamppa
+sudo echo "Pin-Priority: -1" >> /etc/apt/preferences.d/99mozillateamppa
+
 sudo apt update
 sudo apt install -y firefox
