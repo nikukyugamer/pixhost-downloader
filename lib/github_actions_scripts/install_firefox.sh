d91c3bba574feaeaa379b55f84bc23cdc4e9a349
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-12-05 02:47:14 +0900

    ci: 🎡 リポジトリの設定方法をファイルを直接コピーする方法に変更した
    sudo の及ぶ範囲がリダイレクトだと不適切なため

--- lib/github_actions_scripts/install_firefox.sh
+++ lib/github_actions_scripts/install_firefox.sh
@@ -5,14 +5,7 @@
 sudo snap remove --purge firefox
 sudo apt remove --autoremove firefox
 sudo add-apt-repository ppa:mozillateam/ppa
-
-sudo echo "Package: firefox*" >> /etc/apt/preferences.d/99mozillateamppa
-sudo echo "Pin: release o=LP-PPA-mozillateam" >> /etc/apt/preferences.d/99mozillateamppa
-sudo echo "Pin-Priority: 501" >> /etc/apt/preferences.d/99mozillateamppa
-sudo echo "" >> /etc/apt/preferences.d/99mozillateamppa
-sudo echo "Package: firefox*" >> /etc/apt/preferences.d/99mozillateamppa
-sudo echo "Pin: release o=Ubuntu" >> /etc/apt/preferences.d/99mozillateamppa
-sudo echo "Pin-Priority: -1" >> /etc/apt/preferences.d/99mozillateamppa
+sudo cp lib/github_actions_scripts/99mozillateamppa /etc/apt/preferences.d
 
 sudo apt update
 sudo apt install -y firefox
