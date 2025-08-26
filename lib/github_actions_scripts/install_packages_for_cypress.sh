dc79bc917029aa2b0972dd53f47e99a77cd40063
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-08-26 17:47:52 +0900

    ci: 🎡 Fix packages

--- lib/github_actions_scripts/install_packages_for_cypress.sh
+++ lib/github_actions_scripts/install_packages_for_cypress.sh
@@ -3,4 +3,5 @@
 # https://docs.cypress.io/app/get-started/install-cypress#UbuntuDebian
 # name: Cypress の実行に必要なパッケージをインストールする
 # command: |
-sudo apt install -y libgtk2.0-0t64 libgtk-3-0t64 libgbm-dev libnotify-dev libnss3 libxss1 libasound2t64 libxtst6 xauth xvfb
+# sudo apt install -y libgtk2.0-0t64 libgtk-3-0t64 libgbm-dev libnotify-dev libnss3 libxss1 libasound2t64 libxtst6 xauth xvfb
+sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
