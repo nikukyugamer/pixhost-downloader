7c99f67585b2db9b32f42711f8c8cad5264da662
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-08-26 17:53:39 +0900

    Revert "ci: 🎡 Fix libasound2t64"
    This reverts commit 846ab5e065d5acdd9b811f4503b6a0ae2647bc59.

--- lib/github_actions_scripts/install_packages_for_cypress.sh
+++ lib/github_actions_scripts/install_packages_for_cypress.sh
@@ -4,4 +4,4 @@
 # name: Cypress の実行に必要なパッケージをインストールする
 # command: |
 # sudo apt install -y libgtk2.0-0t64 libgtk-3-0t64 libgbm-dev libnotify-dev libnss3 libxss1 libasound2t64 libxtst6 xauth xvfb
-sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2t64 libxtst6 xauth xvfb
+sudo apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
